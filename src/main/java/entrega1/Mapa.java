4fb2cab27fd1b70ab0e9b0724a491d390d4a47d4
Tomas Mendes Ribeiro <tomasmendesr@gmail.com>
2016-05-14 11:44:45 -0300

    Agrego los distintos modificar en Mapa

--- src/main/java/entrega1/Mapa.java
+++ src/main/java/entrega1/Mapa.java
@@ -3,6 +3,8 @@ package entrega1;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+import org.uqbar.geodds.Point;
+
 
 public class Mapa {
 
@@ -43,5 +45,31 @@ public class Mapa {
 		coleccionDePOIS.remove(unPOI);
 	}
 	
-	public void modificarPOI(POI unPOI){}
+	public void modificarUbicacion(POI unPOI, Point nuevaUbicacion){
+		unPOI.setUbicacion(nuevaUbicacion);
+	}
+	
+	public void modificarDireccion(POI unPOI, String nuevaDireccion){
+		unPOI.setDireccion(nuevaDireccion);
+	}
+	
+	public void modificarNombre(POI unPOI, String nuevoNombre){
+		unPOI.setNombre(nuevoNombre);
+	}
+	
+	public void modificarComuna(POI unPOI, Comuna nuevaComuna){
+		unPOI.setComuna(nuevaComuna);
+	}
+	
+	public void modificarRangoDeAtencion(POI unPOI, RangoDeAtencion nuevoRango){
+		unPOI.setRangoDeAtencion(nuevoRango);
+	}
+	
+	public void agregarTag(POI unPOI, String nuevoTag){
+		unPOI.addTag(nuevoTag);
+	}
+	
+	public void quitarTag(POI unPOI, String tag){
+		unPOI.removeTag(tag);
+	}
 }

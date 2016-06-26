05853eef8082723c0a8cc5815c53196ad48d4981
Julian Freire <julislm.13@gmail.com>
2016-06-26 19:41:05 -0300

    Agrego la clase ResultadoProceso y completo Tareas
    Relleno la clase Tareas con los atributos necesarios para el manejo de
    errores por tareas y agrego la clase ResultadoProceso que tiene la
    cantidad de resultados, la fecha y si fallo o no (bool).

---
+++ src/main/java/Procesos/ResultadoProceso.java
@@ -0,0 +1,42 @@
+package Procesos;
+
+import java.time.LocalDateTime;
+
+public class ResultadoProceso {
+	
+	//Constructor
+	public ResultadoProceso(){
+		super();
+	}
+	
+	//Atributos
+	private int cantElementosAfectados;
+	private boolean resultadoDelProceso;
+	private LocalDateTime momentoDeEjecucion;
+	
+	//Getters y Setters
+	
+	public void setCantElementosAfectados(int unaCant){
+		cantElementosAfectados = unaCant;
+	}
+	
+	public void setResultadoDelProceso(boolean value){
+		resultadoDelProceso = value;
+	}
+	
+	public void setMomentoDeEjecucion(LocalDateTime momento){
+		momentoDeEjecucion = momento;
+	}
+	
+	public int getCantElementosAfectados(){
+		return cantElementosAfectados;
+	}
+	
+	public boolean getResultadoDelProceso(){
+		return resultadoDelProceso;
+	}
+	
+	public LocalDateTime getMomentoDeEjecucion(){
+		return momentoDeEjecucion;
+	}
+}
--- src/main/java/Procesos/Tareas.java
+++ src/main/java/Procesos/Tareas.java
@@ -1,5 +1,49 @@
 package Procesos;
 
-public abstract class Tareas {
+import java.time.LocalDateTime;
 
+public abstract class Tareas {
+	
+	//Constructor
+	public Tareas(){
+		super();
+	}
+	
+	//Atributos
+	private int cantARepetir;
+	private boolean enviarMailAdmin;
+	
+	//Getters y Setters
+	public void setCantARepetir(int unaCant){
+		cantARepetir = unaCant;
+	}
+	
+	public void setEnviarMailAdmin(boolean value){
+		enviarMailAdmin = value;		
+	}
+	
+	public int getCantARepetir(){
+		return cantARepetir;
+	}
+	
+	public boolean getEnviarMailAdmin(){
+		return enviarMailAdmin;
+	}
+	
+	//Metodos
+	public ResultadoProceso realizaTarea(){
+		return new ResultadoProceso();
+	}
+	
+	protected ResultadoProceso falle(){
+		ResultadoProceso resultadoProceso = new ResultadoProceso();
+		resultadoProceso.setMomentoDeEjecucion(LocalDateTime.now());
+		resultadoProceso.setCantElementosAfectados(0);
+		resultadoProceso.setResultadoDelProceso(false);
+		while(cantARepetir == 0){
+			resultadoProceso = this.realizaTarea();
+			cantARepetir--;
+		}
+		return resultadoProceso;
+	}
 }

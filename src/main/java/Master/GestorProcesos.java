b284eb9839e64b9262b14baa8fc072d02cdf16c2
Tomas Mendes Ribeiro <tomasmendesr@gmail.com>
2016-06-30 01:51:26 -0300

    Modificaciones en el GestorProcesos

--- src/main/java/Master/GestorProcesos.java
+++ src/main/java/Master/GestorProcesos.java
@@ -1,6 +1,7 @@
 package Master;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -8,14 +9,23 @@ import Procesos.Proceso;
 import Procesos.ResultadoProceso;
 
 public class GestorProcesos {
+	
+	//atributos
+	private List<ResultadoProceso> resultadosProcesos;
+	private List<Proceso> procesos;
 
-
+	public void agregarProcesoAEjecutar(Proceso proceso, Date horario){
+		procesos.add(proceso);
+		this.ejecutarTarea(proceso,horario);
+	}
+	
 	public void ejecutarTarea(Proceso proceso, Date horario){
 		Timer timer = new Timer();
 		TimerTask tareaProgramada = new TimerTask(){
 			@Override
 			public void run(){
-				proceso.realizarProceso();
+				ResultadoProceso resultado = proceso.realizarProceso();
+				resultadosProcesos.add(resultado);
 			}
 		};
 		timer.schedule(tareaProgramada, horario); 
